5c9fa3ec936136994c8f44b4157d3c263523390e
Titus Chang <titus@Tituss-MacBook-Air.local>
2021-11-30 15:29:15 +0000

    Altered named-cat page to POST request

--- app.rb
+++ app.rb
@@ -2,13 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/random-cat' do
-  @random_name = ["Amigo", "Misty", "Almond"].sample
+  @name = ["Amigo", "Misty", "Almond"].sample
   erb :index
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
-  @random_name = params[:name]
+  @name = params[:name]
   erb :index
 end
 
